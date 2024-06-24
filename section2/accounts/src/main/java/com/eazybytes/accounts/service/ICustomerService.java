d3bfdc0fee6c4328188311c18bca636c28374974
zumrudisgandar <zzumus@mail.ru>
2024-06-24 10:46:22 +0400

    Section 8 docs

---
+++ section2/accounts/src/main/java/com/eazybytes/accounts/service/ICustomerService.java
@@ -0,0 +1,14 @@
+package com.eazybytes.accounts.service;
+
+import com.eazybytes.accounts.dto.CustomerDetailsDto;
+
+public interface ICustomerService {
+
+
+    /**
+     *
+     * @param mobileNumber - Input Mobile Number
+     * @return Customer Details based on a given mobileNumber
+     */
+    CustomerDetailsDto fetchCustomerDetails(String mobileNumber);
+}
---
+++ section2/accounts/src/main/java/com/eazybytes/accounts/service/client/LoansFeignClient.java
@@ -0,0 +1,18 @@
+package com.eazybytes.accounts.service.client;
+
+
+import com.eazybytes.accounts.dto.CardsDto;
+import com.eazybytes.accounts.dto.LoansDto;
+import jakarta.validation.constraints.Pattern;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient("loans")
+public interface LoansFeignClient {
+
+    @GetMapping(value = "/api/fetch",consumes = "application/json")
+    public ResponseEntity<LoansDto> fetchLoanDetails(@RequestParam String mobileNumber);
+}
